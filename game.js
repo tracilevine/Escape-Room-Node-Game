347d8cacbf66d9b07053a1ef70864ee29cd0b8dc
tracilevine <televine@gmail.com>
2020-10-12 02:34:00 -0400

    more additions to the game

--- game.js
+++ game.js
@@ -1,48 +1,40 @@
 const readlineSync = require("readline-sync");
 
-const moves = 10; 
+const maxMoves = 10;
+let currentMove =0; 
 const score = 0;
 const life = true;
-let falseItems=["lamp","couch","chair","bike","picture","blanket"];
-let trueItems =["waterbottle","dining room table","notebook","book"];
-let key = "bed" 
+let trueItems=["lamp","couch","chair","bike","picture","blanket"];
+let falseItems =["waterbottle","dining room table","notebook","book"];
+//random item in the true array 
+let key =  trueItems[Math.floor(Math.random()*trueItems.length)];
 
 const name = readlineSync.question('Welcome, guest... what is your name? ');
 
-readlineSync.keyInPause(`
-Welcome to the escape room, ${name}, 
-your goal is to find your way out.
-`);
-readlineSync.keyInPause(`
-In this room you will find somewhere a key that will unlock the door. 
-
+readlineSync.keyInPause(`Welcome to the escape room, ${name}, your goal is to find your way out.`);
+readlineSync.keyInPause(`In this room you will find somewhere a key that will unlock the door. 
 You must look around and find the right tools to find the key. You will have 10 moves to get out of the room! 
 `);
 
 readlineSync.keyInPause(`\nIn this room you are going to find the following items:\n
-a lamp, waterbottle, dining room table, chair, book, notebook, blanket, picture, couch, bike, bed\n`)
-const input = readlineSync.question ("Where do you want to look?\n")
+a lamp, waterbottle, dining room table, chair, book, notebook, blanket, picture, couch, bike, bed\n\n`)
 
-//while statement for if life is true and they still have moves allow .... 
+const userMove = readlineSync.question ("\nWhere do you want to look?\n")
 
-for (let i=0; i<moves; i++){
-    const input = readlineSync.question ("Where do you want to look?\n");
-}
+const yourMoves = (userMove) => {
+    for (let move=0; move <maxMoves; move++){
+        if (userMove === key){
+            console.log ("\nCongratulations! You have one the game!🏆");
+        } else if (userMove === trueItems){
+            console.log(`\nYou are very close....Keep trying....You have ${maxMoves-1} moves left to escape.\n`);
+            currentMove++;
+            return userMove;
+        } else if (userMove === maxMoves -1){
+            console.log(`You lose. 💀\n The key was in the bed!`);
 
-//if input true item  return clue - create loop for true items
+        } else
+            console.log (`\nYou have hit a bomb. Game Over\n`);       
+    }
+};
 
-if (input===trueItems){
-    console.log(`The key is under something soft. You have ${moves-1} left to escape.`);
- } else if (input===key){
-     console.log ("Congratulations! You have one the game!");
- }else{
-     console.log (`Sorry no clue here. You have ${moves-1} left to escape.`);
-     
- };
-
-
-
-
-if (moves === 0 && count == 10) {
-    console.log("You used " + (10 - moves) + " moves and your score was " + (moves * 6) + "\n\n GAME OVER");
-  }
+yourMoves(userMove);
